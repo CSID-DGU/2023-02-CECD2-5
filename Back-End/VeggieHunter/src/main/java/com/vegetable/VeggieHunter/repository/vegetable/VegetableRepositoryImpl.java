e87ce6e249989e5bd43860ef5b0f6f023e301b59
eogus0512 <eogus0512@gmail.com>
2023-11-14 18:15:41 +0900

    feat:채소 마이페이지 기능 구현

--- Back-End/VeggieHunter/src/main/java/com/vegetable/VeggieHunter/repository/vegetable/VegetableRepositoryImpl.java
+++ Back-End/VeggieHunter/src/main/java/com/vegetable/VeggieHunter/repository/vegetable/VegetableRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.vegetable.veggiehunter.repository.vegetable;
 
+import com.querydsl.core.types.Expression;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.PathBuilder;
@@ -7,6 +8,7 @@ import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.vegetable.veggiehunter.domain.Price;
 import com.vegetable.veggiehunter.domain.Vegetable;
+import com.vegetable.veggiehunter.dto.response.likes.VegetableLikesListResponse;
 import com.vegetable.veggiehunter.dto.response.vegetable.VegetableGraphResponse;
 import com.vegetable.veggiehunter.dto.response.vegetable.VegetableListResponse;
 
@@ -98,6 +100,48 @@ public class VegetableRepositoryImpl implements VegetableRepositoryCustom {
                                                         .and(price1.createdDate.eq(yesterdayOfMostRecentDate))
                                                         .and(price1.name.eq(vegetable.name)))
                                 )
+                        )
+                )
+                .from(vegetable)
+                .where(vegetable.id.in(vegetableIdList))
+                .leftJoin(price1).on(price1.unit.eq(vegetable.main_unit)
+                        .and(price1.createdDate.eq(mostRecentDate))
+                        .and(price1.name.eq(vegetable.name)))
+                .groupBy(vegetable.name, vegetable.image, vegetable.main_unit)
+                .fetch();
+    }
+
+    @Override
+    public List<VegetableLikesListResponse> getVegetableLikesList(List<Long> vegetableIdList) {
+        LocalDate mostRecentDate = queryFactory
+                .select(price1.createdDate.max())
+                .from(price1)
+                .fetchOne();
+
+        // 가장 최근 날짜의 어제 날짜 계산
+        LocalDate yesterdayOfMostRecentDate = mostRecentDate.minusDays(1);
+        Boolean isLikes = true;
+
+        return queryFactory
+                .select(
+                        Projections.constructor(
+                                VegetableLikesListResponse.class,
+                                vegetable.id,
+                                vegetable.name,
+                                vegetable.image,
+                                vegetable.main_unit,
+                                price1.price.avg(),
+                                Expressions.numberTemplate(Double.class,
+                                        "({0} - coalesce({1}, 0)) / coalesce({1}, 1)",
+                                        price1.price.avg(),
+                                        JPAExpressions
+                                                .select(price1.price.avg())
+                                                .from(price1)
+                                                .where(price1.unit.eq(vegetable.main_unit)
+                                                        .and(price1.createdDate.eq(yesterdayOfMostRecentDate))
+                                                        .and(price1.name.eq(vegetable.name)))
+                                ),
+                                Expressions.constant(isLikes)
                         )
                 )
                 .from(vegetable)
