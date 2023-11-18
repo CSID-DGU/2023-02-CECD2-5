9d21b09f535d55c8bca36fe6cb5da99835a36dd9
khanz0613 <62611268+khanz0613@users.noreply.github.com>
2023-11-18 11:21:28 +0900

    레시피페이지추가

--- Front-End/vegetable/lib/recipepage.dart
+++ Front-End/vegetable/lib/recipepage.dart
@@ -1,38 +1,59 @@
 import 'package:flutter/material.dart';
-import 'underbar.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'mainpage.dart';
-
-
+import 'dart:convert';
+import 'recipe_list.dart';
 
 class RecipePage extends StatefulWidget {
   @override
   _RecipePageState createState() => _RecipePageState();
 }
 
-class _RecipePageState extends State<RecipePage>{
+class _RecipePageState extends State<RecipePage> {
+  List<dynamic> recipeList = [];
+  
+  @override
+  void initState() {
+    super.initState();
+    fetchRecipes();
+  }
 
+  Future<void> fetchRecipes() async {
+    final url = Uri.parse('http://ec2-54-180-36-184.ap-northeast-2.compute.amazonaws.com:8080/recipe/');
+    final response = await http.get(url);
+    final jsonData = json.decode(response.body);
+
+    if (jsonData['success'] && jsonData['code'] == 200) {
+      var jsonResponse = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        recipeList = jsonResponse['data'];
+      });
+    }
+  }
 
   @override
-  Widget build(AboutDialog){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("레시피",
-        style: TextStyle(color: Colors.black,
-        fontSize: 20),
-        ),
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new, color: Colors.black,),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-
-        ]
-        ),
+        title: Text("레시피 페이지"),
+      ),
+      body: ListView.builder(
+        itemCount: recipeList.length,
+        itemBuilder: (context, index) {
+          var recipe = recipeList[index];
+          return ListTile(
+            title: Text(recipe['name']),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RecipeListPage(vegetableId: recipe['id']),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
+
