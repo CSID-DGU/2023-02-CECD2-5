ccc826bb4a466c27eb9e8a874f57f77995293232
eogus0512 <eogus0512@gmail.com>
2023-11-18 17:32:14 +0900

    feat: 하단바 아이콘 변경

--- Front-End/vegetable/lib/underbar.dart
+++ Front-End/vegetable/lib/underbar.dart
@@ -14,62 +14,77 @@ class CustomBottomBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      selectedItemColor: Colors.green[700],
-      elevation: 10,
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.local_florist, size: 35),
-          label: '채소',
-          
-        ),
-        BottomNavigationBarItem(
-          icon: Stack(
-            alignment: Alignment.center, // 중앙 정렬을 기본으로 설정
-            clipBehavior: Clip.none, // 자식이 경계를 넘어 표시되도록 허용
-            children: [
-              Positioned(
-                top: -35, // 원의 위치를 적절히 조정
-                child: Container(
-                  height: 80, // 원의 크기
-                  width: 80, // 원의 너비
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black ,
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: Offset(0, 3), // 그림자의 위치 (수직으로 3만큼 아래로)
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        selectedItemColor: Color.fromARGB(255, 118, 191, 126),
+        elevation: 10, // 하단바의 그림자 높이 조절
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.eco, size: 35),
+            label: '채소',
+          ),
+          BottomNavigationBarItem(
+            icon: Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: -30,
+                  child: Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromARGB(255, 118, 191, 126),
+                    ),
                   ),
                 ),
-              ),
-              Transform.translate(
-                offset: Offset(0, -15), // 아이콘의 상단 패딩을 적용해 위치를 위로 조정
-                child: Icon(Icons.home, size: 40, color: Colors.white),
-              ),
-            ],
+                Transform.translate(
+                  offset: Offset(0, -14),
+                  child: Image.asset(
+                    'assets/images/home.png',
+                    height: 50,
+                    width: 50,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            label: '',
           ),
-          label: '',
-        ),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.book, size: 35),
-          label: '레시피',
-        ),
-      ],
-      currentIndex: selectedIndex,
-      onTap: (index) {
-        if (index == selectedIndex) {
-          return;
-        }
-        switch (index) {
-          case 0:
-            Navigator.push(context, MaterialPageRoute(builder: (context) => VegetablePage()));
-            break;
-          case 1:
-            Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
-            break;
-          case 2:
-            Navigator.push(context, MaterialPageRoute(builder: (context) => RecipePage()));
-            break;
-        } 
-      }
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant, size: 35),
+            label: '레시피',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          if (index == selectedIndex) {
+            return;
+          }
+          switch (index) {
+            case 0:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => VegetablePage()));
+              break;
+            case 1:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => MainPage()));
+              break;
+            case 2:
+              Navigator.push(context, MaterialPageRoute(builder: (context) => RecipePage()));
+              break;
+          }
+        },
+      ),
     );
   }
 }
